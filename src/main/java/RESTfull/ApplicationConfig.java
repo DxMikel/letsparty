58771b65ee209f986a8c7eda03f4abe3d3211d77
Lesser <Lesser@DESKTOP-7GHOE3U>
2019-07-08 15:04:35 -0500

    ya tiene jersey

--- src/main/java/RESTfull/ApplicationConfig.java
+++ src/main/java/RESTfull/ApplicationConfig.java
@@ -5,31 +5,15 @@
  */
 package RESTfull;
 
-import java.util.Set;
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  *
  * @author Lesser
  */
-@javax.ws.rs.ApplicationPath("webresources")
-public class ApplicationConfig extends Application {
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(RESTfull.BarFacadeREST.class);
-    }
+@ApplicationPath("api")
+public class ApplicationConfig extends Application{
     
 }
+
---
+++ src/main/java/RESTfull/ServicioBar.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RESTfull;
+
+import com.google.gson.Gson;
+import com.letsparty.models.Bar;
+import com.letsparty.servicio.respuesta.RespuestaServicio;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Lesser
+ */
+@Path("/bares")
+public class ServicioBar {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtenerBar(){
+         EntityManager entityManager= null ;
+        try{
+           System.out.println("servicios4");
+                EntityManagerFactory emf = Persistence.createEntityManagerFactory("LetsPartyPU");
+                 entityManager = emf.createEntityManager();
+        List<Bar> bar = entityManager.createQuery("SELECT b FROM Bar b")
+                .getResultList();
+                System.out.println("servicios5");
+                
+                //se agrego para guardarlo en un json
+                String jsonrespuesta= new Gson().toJson(bar);
+                return Response.ok().entity(jsonrespuesta).build();
+     //   return Response.ok().entity(playlist).build();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            if(entityManager != null){
+                if(entityManager.getTransaction() != null){
+                    entityManager.getTransaction().rollback();
+                }
+            }
+            RespuestaServicio rs = new RespuestaServicio();
+                    rs.setCodigo(0);
+                    rs.setMensaje(e.getMessage());
+                    System.out.println("servicios6");
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(rs)
+                    .build();
+        }
+    }
+    
+}
+
