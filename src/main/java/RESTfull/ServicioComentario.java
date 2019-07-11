611ae3503db87bc8ff411e29415f74be7905f4b0
Lesser <Lesser@DESKTOP-7GHOE3U>
2019-07-11 17:59:30 -0500

    reservacion y comentarios api

---
+++ src/main/java/RESTfull/ServicioComentario.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RESTfull;
+
+import com.google.gson.Gson;
+import com.letsparty.models.Comentario;
+import com.letsparty.servicio.respuesta.RespuestaServicio;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Lesser
+ */
+@Path("/comentarios")
+public class ServicioComentario {
+        
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtenerComentario(){
+         EntityManager entityManager= null ;
+        try{
+           //System.out.println("servicios4");
+                EntityManagerFactory emf = Persistence.createEntityManagerFactory("LetsPartyPU");
+                 entityManager = emf.createEntityManager();
+        List<Comentario> comentario = entityManager.createQuery("SELECT c FROM Comentario c")
+                .getResultList();
+                //System.out.println("servicios5");
+                
+                //se agrego para guardarlo en un json
+                String jsonrespuesta= new Gson().toJson(comentario);
+                return Response.ok().entity(jsonrespuesta).build();
+     //   return Response.ok().entity(playlist).build();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            if(entityManager != null){
+                if(entityManager.getTransaction() != null){
+                    entityManager.getTransaction().rollback();
+                }
+            }
+            RespuestaServicio rs = new RespuestaServicio();
+                    rs.setCodigo(0);
+                    rs.setMensaje(e.getMessage());
+                    //System.out.println("servicios6");
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(rs)
+                    .build();
+        }
+    }
+    
+    
+}
---
+++ src/main/java/RESTfull/ServicioReservacion.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RESTfull;
+
+import com.google.gson.Gson;
+
+import com.letsparty.models.Reservacion;
+import com.letsparty.models.Usuario;
+import com.letsparty.servicio.respuesta.RespuestaServicio;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Lesser
+ */
+@Path("/reservaciones")
+public class ServicioReservacion {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtenerReservacion(){
+         EntityManager entityManager= null ;
+        try{
+           //System.out.println("servicios4");
+                EntityManagerFactory emf = Persistence.createEntityManagerFactory("LetsPartyPU");
+                 entityManager = emf.createEntityManager();
+        List<Reservacion> reservacion = entityManager.createQuery("SELECT r FROM Reservacion r")
+                .getResultList();
+            // List<Reservacion>  id_usuario = entityManager.createQuery("SELECT r.id_usuario FROM Reservacion r").getResultList();
+       //reservacion = entityManager.createQuery("SELECT u.nombre u FROM Usuario u where u.id_usuario = " + id_usuario )
+           //     .getResultList();
+         //       System.out.println(id_usuario);
+                
+                //se agrego para guardarlo en un json
+                String jsonrespuesta= new Gson().toJson(reservacion);
+                return Response.ok().entity(jsonrespuesta).build();
+     //   return Response.ok().entity(playlist).build();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            if(entityManager != null){
+                if(entityManager.getTransaction() != null){
+                    entityManager.getTransaction().rollback();
+                }
+            }
+            RespuestaServicio rs = new RespuestaServicio();
+                    rs.setCodigo(0);
+                    rs.setMensaje(e.getMessage());
+                    //System.out.println("servicios6");
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(rs)
+                    .build();
+        }
+    }
+    
+}
+    
+
